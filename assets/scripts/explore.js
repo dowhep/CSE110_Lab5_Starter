92d14aba01739c5ac477a822b568d767c769ece5
Marcelo Shen <caogangshen@gmail.com>
2023-05-07 23:52:09 -0700

    Completed explore and expose part 1

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,66 @@
 // explore.js
-
 window.addEventListener('DOMContentLoaded', init);
+const synth = window.speechSynthesis;
+let speakSelect;
+let voices;
+let voiceOptions = [];
 
 function init() {
-  // TODO
+
+  // Assign all elements to a variable
+  const section = document.getElementById("explore");
+  const assetImg = section.getElementsByTagName("img")[0];
+  const speakText = document.getElementById("text-to-speak");
+  speakSelect = document.getElementById("voice-select");
+  const speakBtn = section.getElementsByTagName("button")[0];
+
+  // delay to ensure synth is loaded
+  synth.addEventListener("voiceschanged", populateVoiceList);
+
+  // play voice on button click
+  speakBtn.addEventListener("click", (event) => {
+    if (speakSelect.value == "select") return;
+
+    // stop previous utterance 
+    synth.cancel();
+    const utterThis = new SpeechSynthesisUtterance(speakText.value);
+    let indexSelected = speakSelect.selectedIndex - 1;
+    utterThis.voice = voices[indexSelected];
+    
+    // change face
+    utterThis.addEventListener("start", (event) => {
+      assetImg.src = "assets/images/smiling-open.png";
+    });
+    utterThis.addEventListener("end", (event) => {
+      assetImg.src = "assets/images/smiling.png";
+    });
+    synth.speak(utterThis);
+  });
+}
+
+
+function populateVoiceList() {
+  // Populate Voice List
+  voices = synth.getVoices();
+
+  // Clear OG voice
+  for (let option of voiceOptions) {
+    option.remove();
+  }
+  voiceOptions = [];
+
+  // Update current voice
+  for (let i = 0; i < voices.length; i++) {
+    const option = document.createElement("option");
+    option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+    if (voices[i].default) {
+      option.textContent += " — DEFAULT";
+    }
+
+    option.setAttribute("data-lang", voices[i].lang);
+    option.setAttribute("data-name", voices[i].name);
+    speakSelect.appendChild(option);
+    voiceOptions.push(option);
+  }
 }
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,7 +1,52 @@
 // expose.js
+const jsConfetti = new JSConfetti();
 
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  // Assign all elements to a variable
+  let section = document.getElementById("expose");
+  let assetImg = section.getElementsByTagName("img")[0];
+  let assetSelect = document.getElementById("horn-select");
+  
+  let volumeControl = document.getElementById("volume-controls");
+  let volumeControlInput = volumeControl.getElementsByTagName("input")[0];
+  let volumeControlImg = volumeControl.getElementsByTagName("img")[0];
+
+  let soundBtn = section.getElementsByTagName("button")[0];
+  let soundSrc = section.getElementsByTagName("audio")[0];
+
+  // select horn from dropdown
+  assetSelect.addEventListener("change", (event) => {
+    let prefix = event.target.value;
+    if (prefix == "select") return;
+    
+    assetImg.src = "assets/images/" + prefix + ".svg";
+    soundSrc.src = "assets/audio/" + prefix + ".mp3";
+  });
+
+  // initialize volume to 50%
+  soundSrc.volume = volumeControlInput.value / 100;
+  // change volume with slider
+  volumeControlInput.addEventListener("change", (event) => {
+    let value = event.target.value;
+    soundSrc.volume = value / 100;
+    if (value == 0) {
+      volumeControlImg.src = "assets/icons/volume-level-0.svg";
+    } else if (value < 33) {
+      volumeControlImg.src = "assets/icons/volume-level-1.svg";
+    } else if (value < 67) {
+      volumeControlImg.src = "assets/icons/volume-level-2.svg";
+    } else {
+      volumeControlImg.src = "assets/icons/volume-level-3.svg";
+    }
+  });
+
+  // play sound when clicked
+  soundBtn.addEventListener("click", (event) => {
+    if (assetSelect.value == "select") return;
+    soundSrc.currentTime = 0;
+    soundSrc.play();
+    if (assetSelect.value == "party-horn") jsConfetti.addConfetti();
+  });
 }
